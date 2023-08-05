d55eb4b5655941c66b1e48a3ee82996be0798b49
Druyeah <you@example.com>
2023-08-04 21:42:30 -0400

    subo tarea opcional de tienda de frutas

---
+++ Optional/dojo_fruit_store-master/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)  
+app.secret_key = 'contrasena secreta'
+
+@app.route('/')         
+def index():
+    return render_template("index.html")
+
+@app.route('/checkout', methods=['POST'])         
+def checkout():
+    
+    print(request.form)
+    session["strawberry"] = request.form["strawberry"]
+    session["raspberry"] = request.form["raspberry"]
+    session["apple"] = request.form["apple"]
+    session["first_name"] = request.form["first_name"]
+    session["last_name"] = request.form["last_name"]
+    session["student_id"] = request.form["student_id"]
+    
+    total_frutas = int(session["strawberry"]) + int(session["raspberry"]) + int(session["apple"])
+    
+    print(f"Cobrando a {session['first_name']} {session['last_name']} por {total_frutas} frutas")
+
+    return render_template("checkout.html")
+
+@app.route('/fruits')         
+def fruits():
+    return render_template("fruits.html")
+
+if __name__=="__main__":   
+    app.run(debug=True)    
